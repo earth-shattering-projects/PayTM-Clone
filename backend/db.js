9a691630bf0015c43457fe261f3ac816f302419f
Divyam Patro <divyampatro1997@gmail.com>
2024-01-30 11:23:51 -0600

    Added accounts table to db

--- backend/db.js
+++ backend/db.js
@@ -37,6 +37,14 @@ const UserSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", UserSchema);
 
+const AccountSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  balance: { type: Number, required: true },
+});
+
+const Account = mongoose.model("Account", AccountSchema);
+
 module.exports = {
   User,
+  Account,
 };
