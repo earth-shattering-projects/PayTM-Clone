f7917a2a694951f98f6c7be962dada83b7b4ff6b
Divyam Patro <divyampatro1997@gmail.com>
2024-02-04 12:24:15 -0600

    Designed the signup and signin pages and associated components

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Heading from "../UI/Heading";
+import SubHeading from "../UI/SubHeading";
+import InputBox from "../UI/InputBox";
+import Button from "../UI/Button";
+import BottomWarning from "../UI/BottomWarning";
 
 const Signin = () => {
-  return <div>Signin</div>;
+  return (
+    <div className="bg-slate-300 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+          <Heading label={"Sign In"} />
+          <SubHeading label={"Enter your credentials to access your account"} />
+          <InputBox label={"Email"} placeholder={"johndoe@example.com"} />
+          <InputBox label={"Password"} placeholder={"123456"} />
+          <Button label={"Sign In"} />
+          <BottomWarning
+            label={"Don't have an account?"}
+            buttonText={"Sign Up"}
+            to={"/signup"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Heading from "../UI/Heading";
+import SubHeading from "../UI/SubHeading";
+import InputBox from "../UI/InputBox";
+import Button from "../UI/Button";
+import BottomWarning from "../UI/BottomWarning";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="bg-slate-300 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+          <Heading label={"Signup"} />
+          <SubHeading label={"Enter your information to create an account"} />
+          <InputBox label={"First Name"} placeholder={"John"} />
+          <InputBox label={"Last Name"} placeholder={"Doe"} />
+          <InputBox label={"Email"} placeholder={"johndoe@example.com"} />
+          <InputBox label={"Password"} placeholder={"123456"} />
+          <Button label={"Sign up"} />
+          <BottomWarning
+            label={"Already have an account?"}
+            buttonText={"Sign in"}
+            to={"/signin"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
